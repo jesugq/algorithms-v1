a065dc3477b40a37f5c69d05b358c8375dd42dd0
jesugq <jesugq@gmail.com>
2020-11-17 17:08:05 -0600

    Remove Java folder

--- leetcode/.java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] unsortedNumbers = {8, 1, 2, 2, 3};
-        int[] smallerNumbers = solution.smallerNumbersThanCurrent(unsortedNumbers);
-        System.out.println(Arrays.toString(smallerNumbers));
-    }
-}
